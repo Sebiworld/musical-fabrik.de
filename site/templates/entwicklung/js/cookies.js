3dcbeed95d342abc65b50733279356ce7b43c2c7
Sebastian Schendel <sebi@sebi.ninja>
2019-01-31 17:02:24 +0100

    Matomo Code angepasst

--- site/templates/entwicklung/js/cookies.js
+++ site/templates/entwicklung/js/cookies.js
@@ -3,23 +3,16 @@
 
 	if (!(window.location.hostname.indexOf(".local") >= 0)) {
 		// Matomo-Tracking
-		var _paq = _paq || [];
-		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-		_paq.push(["setDoNotTrack", true]);
-		_paq.push(["trackPageView"]);
-		_paq.push(["enableLinkTracking"]);
+		  var _paq = window._paq || [];
+		  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		  _paq.push(['trackPageView']);
+		  _paq.push(['enableLinkTracking']);
 		(function() {
 			var u = "//statistik.musical-fabrik.de/";
-			_paq.push(["setTrackerUrl", u + "piwik.php"]);
+			_paq.push(["setTrackerUrl", u + "matomo.php"]);
 			_paq.push(["setSiteId", "1"]);
-			var d = document,
-				g = d.createElement("script"),
-				s = d.getElementsByTagName("script")[0];
-			g.type = "text/javascript";
-			g.async = true;
-			g.defer = true;
-			g.src = u + "piwik.js";
-			s.parentNode.insertBefore(g, s);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			 g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 		})();
 
 		function matomoTracking(aktivieren) {
